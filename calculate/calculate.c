24a2e9ce613a4d95ddb1fd6923c306e1026f820f
Thembaletu Mba <khodingtata@gmail.com>
2025-12-26 04:10:29 -0500

    Running C with / from Rust

---
+++ calculate/calculate.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int solve(char *line, int *solution);
+
+int main() {
+    char line[100];
+    int solution;
+
+    while (1) {
+    printf("> ");
+    if (fgets(line, 100, stdin) == NULL) {
+        return 0;
+    }
+
+    if (solve(line, &solution)) {
+        continue;
+    }
+
+    printf("%d\n", solution);
+    
+    }
+    return 0;
+}
--- calculate/src/lib.rs
+++ calculate/src/lib.rs
@@ -1,15 +1,62 @@
 use libc::{c_char, c_int};
 use std::ffi::CStr;
 
-/*
+
+// Updating the solve function to call the new evealuate func
+
+enum Error {
+    
+}
+
+fn evaluate(problem: &str) -> Result<i32, Error> {
+    Ok(1)
+}
+
+
 #[no_mangle]
 
 pub extern "C" fn solve(line: *const c_char, solution: *mut c_int) -> c_int {
-    0
-}
- */
+      if line.is_null() || solution.is_null() {
+         return 1;
+        } // returning a Cstr AND the solution in int
+        
+        let c_str = unsafe { CStr::from_ptr(line) };
+        let r_str = match c_str.to_str() {
+            Ok(s) => s,            
+            Err(e) => {
+                eprintln!("UTF-8 Error: {}", e);
+                return 1;
+            },
+        };
 
-#[no_mangle]
-pub extern "C" fn add(a: c_int, b: c_int) -> c_int {
-    a + b
+        match evaluate(r_str) {
+            Ok(value) => {
+                unsafe {
+                    *solution = value as c_int;
+                }
+                0       
+            }
+            Err(e) => {
+                eprintln!("Error");
+
+                1
+            }
+        }
+
+   /*  println!("line: {}", r_str);
+        
+        println!("r_str.as_ptr(): line: {:p}", 
+        //r_str.as_ptr(), line);
+        
+         unsafe {
+            *solution = 1024;
+
+        }
+                    
+       0
+
+       */
 }
+
+
+
